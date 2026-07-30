fcefcc3f9d1458285d8a2c9ae7e789a923047fb3
Roflshockie <102290685+Roflsockie@users.noreply.github.com>
2026-07-30 19:44:52 +0300

    Add js/config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  SUPABASE_URL: 'https://hcezsfcaxkxehleqxfis.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_cSr8Z8xS-Pgplcz6gJg06w_HMZ62AkM',
+
+  MANAGER_USER_IDS: ['285153745778638848']
+}
